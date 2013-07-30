ed698f20591a31b869a2cba3e363659f391bb65e
Joe Koberg <joe.koberg@versionone.com>
2013-07-30 10:56:25 -0400

    Allow downloading of Nuget.exe at build time (from target in Nuget.targets)

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 # ./NuGet.exe install -OutputDirectory packages ./OAuth2Client/packages.config
 
-/c/windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe  OAuth2Client.sln
-
+/c/windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe //p:DownloadNuGetExe=true  OAuth2Client.sln
+ 
