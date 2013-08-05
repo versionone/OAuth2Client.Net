00203a672f73992bd440c85c6c19e99eb3e45f08
Joe Koberg <joe.koberg@versionone.com>
2013-08-05 17:13:32 -0400

    cleanup

--- OAuth2Client/Flows.fs
+++ OAuth2Client/Flows.fs
@@ -2,30 +2,49 @@
 
 
 open System
+open System.Net
+
+open System.Text
+open System.Threading.Tasks
 
 
-type Flow(storage:Storage.JsonFiles) = 
 
-  member this.GetOutOfBand(secrets:Secrets, scope) =
-    Console.SetIn(new IO.StreamReader(Console.OpenStandardInput(8192)));
+(*
+
+
+type Flow(authCodeUI: IAuthCodeRetrievalUI, storage:Storage.JsonFiles, scope:string) = 
+
+  member this.GetOutOfBand(secrets:Secrets) = async {
     let client = AuthClient(secrets, scope)
-    printfn "Please visit this url to authorize the permissions:\n\n%s" (client.getUrlForGrantRequest())
-    printfn "\nPaste the code here:"
-    let code = Console.ReadLine()
-    client.exchangeAuthCode(code)
+    let! code = authCodeUI.GetAuthCode(client.getUrlForGrantRequest())
+    return! client.exchangeAuthCode(code)
+    }
 
-  member this.GetWeb(secrets:Secrets, scope) = 
-    failwith "NotImplemented"
 
-  member this.Start(scope) = async {
-
-    let! secrets = async {
+  member this.GetWeb(secrets:Secrets) : Async<string * Async<Credentials>> = async {
+    let client = AuthClient(secrets, scope)
+    let mypath = secrets.redirect_uris
+    let credentials = async {
+      let urls = [for url in secrets.redirect_uris -> url.ToString()]
+      let! httprequest =  this.ListenForOauth2Code(urls)
+      let! code, state = httprequest.ReadAuthCode()
       try
-        return! storage.GetSecrets()
+        let! creds = client.exchangeAuthCode(code)
+        do! httprequest.Respond(200, "<p>Auth code has been exchanged for a persistent token. You may close this browser window.</p>")
+        return creds
       with
-      | :? IO.IOException as ex ->
-        return! failwith (sprintf "Exception %A reading secrets file %s. Please download or create secrets file and try again" ex storage.SecretsFilename)
+        | :? WebException as ex ->
+          do! httprequest.Respond(500, "Unable to exchange auth code. " + ex.Message)
+          return failwith "Couldn't exchange credentials"
       }
+    return client.getUrlForGrantRequest(), credentials
+    }
+
+
+  member this.Start(secretsStorage:ISecretsStorage) = async {
+    let! secrets = secretsStorage.GetSecrets() |> Async.AwaitTask
+    let! credentials = credentialsStorage.
+
 
     let! credentials = async {
       try
@@ -34,10 +53,18 @@ type Flow(storage:Storage.JsonFiles) =
       | :? System.IO.IOException as ex ->
         let! creds = 
           match secrets.client_type with
-          | Installed -> this.GetOutOfBand(secrets, scope)
-          | Web -> this.GetWeb(secrets, scope)
+          | Installed -> this.GetOutOfBand(secrets)
+          | Web -> async {
+              let! grantUrl, creds = this.GetWeb(secrets)
+              printfn "\nStarted HTTP server to listen for auth code."
+              printfn "The granting server must be able to hit this machine for the callback to succeed."
+              printfn "\nTo grant permissions, visit URL:\n\n  %s\n\n" grantUrl
+              return! creds
+              }
         return! storage.StoreCredentials(creds)
         }
-
     return secrets, credentials
     }
+
+
+    *)
