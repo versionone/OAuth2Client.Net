cc83b2cfe5568da70ba0e48ededbe1766cae7295
Joe Koberg <joe.koberg@versionone.com>
2013-08-06 10:31:12 -0400

    Trying to get async to work under console

--- GrantTool/Program.fs
+++ GrantTool/Program.fs
@@ -14,27 +14,35 @@ let askForAuthCode (client:AuthClient) =
 
 
 let asyncMain (argv:string[]) = async {
+  try
 
-  let scope, secretsFilename, credsFilename = 
-    match argv.Length with
-    | 2 -> argv.[1], "client_secrets.json", "stored_credentials.json"
-    | 3 -> argv.[1], argv.[2], "stored_credentials.json"
-    | 4 -> argv.[1], argv.[2], argv.[3]
-    | _ -> failwith "Usage: %s <scope> [<secretsfile>] [<credsfile>]" argv.[0]
+    let scope, secretsFilename, credsFilename = 
+      match argv.Length with
+      | 1 -> argv.[0], "client_secrets.json", "stored_credentials.json"
+      | 2 -> argv.[0], argv.[1], "stored_credentials.json"
+      | 3 -> argv.[0], argv.[1], argv.[2]
+      | _ -> failwith "Usage: GrantTool.exe <scope> [<secretsfile>] [<credsfile>]"
 
-  let secrets = Secrets.FromJson(File.ReadAllText(secretsFilename, Text.Encoding.UTF8))
+    let secrets = Secrets.FromJson(File.ReadAllText(secretsFilename, Text.Encoding.UTF8))
 
-  let client = AuthClient(secrets, scope)
-  let code = askForAuthCode client
-  let! creds = Async.AwaitTask <| client.exchangeAuthCode code
+    let client = AuthClient(secrets, scope)
+    let code = askForAuthCode client
+    let! creds = Async.AwaitTask <| client.exchangeAuthCode code
 
-  File.WriteAllText(credsFilename, creds.ToJson())
+    File.WriteAllText(credsFilename, creds.ToJson())
 
-  printfn "Successfully saved credentials to %s" credsFilename
-  return 0
+    printfn "Successfully saved credentials to %s" credsFilename
+    return 0
+
+  with :? Exception as ex ->
+    printfn "%s" (ex.StackTrace.ToString())
+    printfn "Failed.\n%s" ex.Message
+    ignore(Console.ReadLine())
+    return 1
   }
 
+open Nito.AsyncEx
 
 [<EntryPoint>]
-let main =  asyncMain >> Async.RunSynchronously
+let main argv =  AsyncContext.Run(Async.StartAsTask(asyncMain argv))
 
