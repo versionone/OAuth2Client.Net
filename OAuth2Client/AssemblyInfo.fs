438af7e1fe8a9eeb6160fde1d65f50da8722cff3
Joe Koberg <joe.koberg@versionone.com>
2013-08-12 15:48:32 -0400

    bump version

--- OAuth2Client/AssemblyInfo.fs
+++ OAuth2Client/AssemblyInfo.fs
@@ -16,8 +16,8 @@ open System.Reflection
 [<assembly: AssemblyCompany("VersionOne, Inc.")>]
 [<assembly: AssemblyProduct("VersionOne")>]
 [<assembly: AssemblyCopyright("Copyright 2013, VersionOne, Inc. Please see the LICENSE.MD file.")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.0.1.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.1.0")>]
 
 
 ignore ()
