a8f8a5f0a756c4d41b6f582d270c56cf679cf15d
Joe Koberg <joe.koberg@versionone.com>
2013-08-12 10:55:42 -0400

    Move storage interfaces to Storage.fs

--- OAuth2Client/Storage.fs
+++ OAuth2Client/Storage.fs
@@ -1,4 +1,6 @@
-﻿module OAuth2Client.Storage
+﻿namespace OAuth2Client
+
+open System.Threading.Tasks
 
 open FSharp.Data.Json
 open FSharp.Data.Json.Extensions
@@ -9,47 +11,61 @@ open System
 
 open Extensions.File
 
-type Options = { secretsFile: string; credsFile: string } with
+type IStorageAsync =
+  abstract member GetSecretsAsync : unit -> Task<Secrets>
+  abstract member GetCredentialsAsync : unit -> Task<Credentials>
+  abstract member StoreCredentialsAsync : Credentials -> Task<Credentials>
 
-  static member Default =
-    { secretsFile = "client_secrets.json"
-      credsFile = "stored_credentials.json" }
+  
+type IStorage =
+  abstract member GetSecrets : unit -> Secrets
+  abstract member GetCredentials : unit -> Credentials
+  abstract member StoreCredentials : Credentials -> Credentials
+  
 
-  static member FromEnvironment =
-    let envSecrets = Environment.GetEnvironmentVariable("V1SDK_SECRETS")
-    let envCreds = Environment.GetEnvironmentVariable("V1SDK_CREDS")
-    { secretsFile = if envSecrets = null then Options.Default.secretsFile else envSecrets
-      credsFile = if envCreds = null then Options.Default.credsFile else envCreds
-      }
+
+module Storage = 
+  type Options = { secretsFile: string; credsFile: string } with
+
+    static member Default =
+      { secretsFile = "client_secrets.json"
+        credsFile = "stored_credentials.json" }
+
+    static member FromEnvironment =
+      let envSecrets = Environment.GetEnvironmentVariable("V1SDK_SECRETS")
+      let envCreds = Environment.GetEnvironmentVariable("V1SDK_CREDS")
+      { secretsFile = if envSecrets = null then Options.Default.secretsFile else envSecrets
+        credsFile = if envCreds = null then Options.Default.credsFile else envCreds
+        }
      
 
-type JsonFileStorage(secretsFileName:string, credFileName:string) =
-  interface IStorage with
-    override this.GetSecrets() = 
-      Secrets.FromJson <| File.ReadAllText(secretsFileName)
+  type JsonFileStorage(secretsFileName:string, credFileName:string) =
+    interface IStorage with
+      override this.GetSecrets() = 
+        Secrets.FromJson <| File.ReadAllText(secretsFileName)
 
-    override this.GetCredentials() = 
-      Credentials.FromJson <| File.ReadAllText(credFileName)
+      override this.GetCredentials() = 
+        Credentials.FromJson <| File.ReadAllText(credFileName)
 
-    override this.StoreCredentials(creds:Credentials) = 
-      File.WriteAllText(credFileName,  creds.ToJson())
-      creds
+      override this.StoreCredentials(creds:Credentials) = 
+        File.WriteAllText(credFileName,  creds.ToJson())
+        creds
 
-  interface IStorageAsync with
-    override this.GetSecretsAsync() = Async.StartAsTask <| async {
-      let! text = File.ReadTextAsync(secretsFileName)
-      return Secrets.FromJson(text)
-      }
+    interface IStorageAsync with
+      override this.GetSecretsAsync() = Async.StartAsTask <| async {
+        let! text = File.ReadTextAsync(secretsFileName)
+        return Secrets.FromJson(text)
+        }
 
-    override this.GetCredentialsAsync() = Async.StartAsTask <| async {
-      let! text = File.ReadTextAsync(credFileName)
-      return Credentials.FromJson(text)
-      }
+      override this.GetCredentialsAsync() = Async.StartAsTask <| async {
+        let! text = File.ReadTextAsync(credFileName)
+        return Credentials.FromJson(text)
+        }
       
-    override this.StoreCredentialsAsync(creds:Credentials) = Async.StartAsTask <| async {
-      let! result = File.WriteTextAsync(credFileName, creds.ToJson())
-      return creds
-      }
+      override this.StoreCredentialsAsync(creds:Credentials) = Async.StartAsTask <| async {
+        let! result = File.WriteTextAsync(credFileName, creds.ToJson())
+        return creds
+        }
 
-  static member Default = JsonFileStorage(Options.FromEnvironment.secretsFile, Options.FromEnvironment.credsFile)
+    static member Default = JsonFileStorage(Options.FromEnvironment.secretsFile, Options.FromEnvironment.credsFile)
 
