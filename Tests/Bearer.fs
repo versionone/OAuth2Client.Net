9cbcdecf94359d69f104fa89f0c08dac5ecbeaba
Joe Koberg <joe.koberg@versionone.com>
2013-08-20 16:25:43 -0400

    Remove extraneous test

--- Tests/Bearer.fs
+++
@@ -1,7 +0,0 @@
-﻿module OAuth2ClientTests.Bearer
-
-open NUnit.Framework
-
-
-type Tests =
-  [<Test>] member this.``a request to a protected endpoint without auth returns the scopes`` () = failwith "Not Implemented"
