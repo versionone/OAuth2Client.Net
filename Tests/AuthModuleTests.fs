de6ee5337c087fcc8532ab41713fe9f36100229e
Joe Koberg <joe.koberg@versionone.com>
2013-12-05 12:45:08 -0500

    Auth Module tests

--- Tests/AuthModuleTests.fs
+++ Tests/AuthModuleTests.fs
@@ -4,14 +4,17 @@ open System.Net
 open NUnit.Framework
 open OAuth2Client
 
-let SECRETS = @"C:\Users\JKoberg\support\ssotest\localhost\client_secrets.json"
-let CREDS = @"C:\Users\JKoberg\support\ssotest\localhost\stored_credentials.json"
-let TESTURL = "http://jkoberg1/VersionOne.Web/rest-1.v1/Data/Member"
 
-let [<Test>] ``The authorization module is usable`` () =
+
+let TESTURL = "http://localhost/VersionOne.Web/rest-1.v1/Data/Member"
+
+/// This test depends on suitable client_secrets.json and stored_credentials.json
+/// files being present and suitable for an instance currently running on localhost/VersionOne.Web
+
+let [<Test>] ``The authorization module works with a System.Net.WebRequest`` () =
   AuthenticationManager.Unregister("basic")
   AuthenticationManager.Register(OAuth2BearerModule())
-  let creds = OAuth2Credentials("apiv1", Storage.JsonFileStorage(SECRETS, CREDS))
+  let creds = OAuth2Credentials("apiv1", Storage.JsonFileStorage.Default)
 
   let req = WebRequest.CreateHttp(TESTURL)
   req.Credentials <- creds
@@ -22,9 +25,12 @@ let [<Test>] ``The authorization module is usable`` () =
   let body = stream.ReadToEnd()
   
   // with the System.Net client, the first request will NOT pre-authenticate
-  // because it wants to see an Authorization from a successful Authenticate
+  // because it wants to first see an Authorization from a successful Authenticate()
   // to do so. So we run the request twice just to verify the second time does
   // properly pre-authenticate and doesn't refresh.
+
+  // Uh, i guess we dont actually check that here but this test will drive
+  // traffic that can be checked with a packet capture
   let req = WebRequest.CreateHttp(TESTURL)
   req.Credentials <- creds
   req.PreAuthenticate <- true
